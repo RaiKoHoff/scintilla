abcef67dc55ca9078a3bcd245ae91132d80303d6
Rainer Kottenhoff <rainer.kottenhoff@gmail.com>
2019-11-15 09:59:05 +0100

    + upd: merge current NP3 dev

--- lexers/LexBatch.cxx
+++ lexers/LexBatch.cxx
@@ -137,6 +137,29 @@ static void ColouriseBatchLine(
 			styler.ColourTo(endPos, SCE_BAT_COMMENT);
 			return;
 		}
+		else if ((wbl > 2) && (wordBuffer[0] == '&') && (wordBuffer[1] == ':') && (wordBuffer[2] == ':') && (continueProcessing)) {
+			styler.ColourTo(endPos, SCE_BAT_COMMENT);
+			return;
+		}
+		else if (wordBuffer[0] == '&') {
+			// check for comment
+			Sci_PositionU cmntLoc = offset - wbl + 1;
+			// Skip next spaces
+			while ((cmntLoc < lengthLine) &&
+				(isspacechar(lineBuffer[cmntLoc]))) {
+				cmntLoc++;
+			}
+			int p = 0;
+			char buffer[5];
+			while ((cmntLoc < lengthLine) && (p < 4)) {
+				buffer[p++] = lineBuffer[cmntLoc++];
+			}
+			buffer[p] = '\0';
+			if ((((buffer[0] == ':') && (buffer[1] == ':')) || (CompareCaseInsensitive(buffer, "rem ") == 0)) && (continueProcessing)) {
+				styler.ColourTo(endPos, SCE_BAT_COMMENT);
+				return;
+			}
+		}
 		// Check for Separator
 		if (IsBSeparator(wordBuffer[0])) {
 			// Check for External Command / Program
